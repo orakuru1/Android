a335ecceea74281c8c448cefd43ad40f9a5289cb
Kameiyuto^ <kamei.11y17@gmail.com>
2025-09-18 10:45:27 +0900

    タイトルbgm

--- Quizkadai/app/src/main/java/com/exampter/test/TitleActivity.kt
+++ Quizkadai/app/src/main/java/com/exampter/test/TitleActivity.kt
@@ -1,6 +1,7 @@
 package com.exampter.test
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -13,9 +14,17 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.FirebaseDatabase
 
 class TitleActivity : AppCompatActivity() {
+    private var bgmPlayer: MediaPlayer? =null  //BGM用のプレイヤー
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_title)
+
+        //bgm再生の準備
+        bgmPlayer = MediaPlayer.create(this, R.raw.kuizetitle)
+        bgmPlayer?.isLooping = true //ループ再生
+        bgmPlayer?.start()
+
 
         val startButton = findViewById<Button>(R.id.startButton)        //ボタン操作できるように
         //ボタンをクリックしたときにおこることを登録
@@ -28,4 +37,34 @@ class TitleActivity : AppCompatActivity() {
             finish()
         }
     }
+
+    override fun onPause()
+    {
+        super.onPause()
+        //画面を離れたら音を止める
+        if(bgmPlayer?.isPlaying == true)
+        {
+            bgmPlayer?.pause()
+        }
+    }
+
+    override fun onResume()
+    {
+        super.onResume()
+        //戻ってきたら音を再開
+        if(bgmPlayer?.isPlaying ==false)
+        {
+            bgmPlayer?.start()
+        }
+    }
+
+    override fun onDestroy()
+    {
+        super.onDestroy()
+        //リソース開放
+        bgmPlayer?.release()
+        bgmPlayer = null
+    }
+
+
 }
